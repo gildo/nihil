9b389e704eb2c93bf49c8b0ceb2c08ea7e311f7b
FL3 <fl3@FL3.homenet.telecomitalia.it>
2010-01-26 16:02:38 +0100

    modifche apportate all'install.php

--- install.php
+++ install.php
@@ -1,6 +1,7 @@
 <?php
 
     include("config.php");
+    include("sources/core.php");
 
     $hey = mysql_connect($host,$username,$password);
     mysql_select_db($database, $hey);
@@ -36,7 +37,28 @@
             	PRIMARY KEY (`id`)
         );
     ");
-
+    
+    print "<from action = '' method = 'POST'>";
+    print "<p>username admin: <input type = 'text' name = 'username'></p>";
+    print "<p>password admin: <input type = 'password' name = 'password'></p>";
+    print "<p>repeat password: <input type = 'password' name = 'repeat'></p>";
+    print "<p>email admin: <input type = 'text' name = 'email'></p>";
+    print "</form>";
+    
+    if(!empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['repeat']) && eregi("^[_a-z0-9+-]+(\.[_a-z0-9+-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)+$",$_POST['email']){
+    	$username = htmlentities($_POST['username']);
+    	$password = htmlentities($_POST['password']);
+    	$repeat   = htmlentities($_POST['repeat']);
+    	$email    = $_POST['email'];
+    	$power    = 'admin';
+    	
+    	if($password == $repeat){
+    		register($username,$password,$email,$power);
+		}else{
+			print "Passwords do not match";
+		}
+	}
+	
     mysql_close($hey);
 
 ?>
