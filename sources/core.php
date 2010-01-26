11a90ddaefe0a04fb981f73341e8fe26c57ce036
Fyskij <fyskij@gmail.com>
2010-01-26 16:25:27 +0100

    fix

--- sources/core.php
+++ sources/core.php
@@ -4,71 +4,72 @@
 
     function login($username,$password){
 
-    	$password = md5(sha1($password));
-    	$query    = "SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}'";
-    	$res      = mysql_query($query) or die ("SQL error:".mysql_error());
-    	$rows     = mysql_num_rows($res);
-    	if($res != 1)
-    	{
-    		print "Wrong username or password";
-    	}
+        $password = md5(sha1($password));
+        $query    = "SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}'";
+        $res      = mysql_query($query) or die ("SQL error:".mysql_error());
+        $rows     = mysql_num_rows($res);
+        if($res != 1)
+        {
+            print "Wrong username or password";
+        }
 
-    	else
-    	{
-    		print "Login lates with success";
-    		setcookie('login',$password,time()+2000,'/');
-    	}
+        else
+        {
+            print "Login lates with success";
+            setcookie('login',$password,time()+2000,'/');
+        }
     }
 
     //is_admin ->
 
     function is_admin($username,$password){
 
-    	$password = md5(sha1($password));
-    	$query    = "SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}' AND level = 'admin'";
-    	$res      = mysql_query($query) or die ("SQL error:".mysql_error());
-    	$rows     = mysql_num_rows($res);
+        $password = md5(sha1($password));
+        $query    = "SELECT * FROM users WHERE username = '{$username}' AND password = '{$password}' AND level = 'admin'";
+        $res      = mysql_query($query) or die ("SQL error:".mysql_error());
+        $rows     = mysql_num_rows($res);
 
-    	if($res != 1)
-    	{
-    		print "Wrong username or password";
-    	}
+        if($res != 1)
+        {
+            print "Wrong username or password";
+        }
 
-    	else
-    	{
-    		print "Login lates with success, hi admin";
-    		setcookie('login',$password,time()+2000,'/');
-    	}
+        else
+        {
+            print "Login lates with success, hi admin";
+            setcookie('login',$password,time()+2000,'/');
+        }
     }
 
     //register ->
 
     function register($username,$password,$email,$level){
-    	$password = md5(sha1($password));
-    	$control  = "SELECT * FROM users WHERE password = '{$password}'";
-    	$res      = mysql_query($control) or die ("SQL error:".mysql_error());
-    	$rows     = mysql_num_rows($res);
+        $password = md5(sha1($password));
+        $control  = "SELECT * FROM users WHERE password = '{$password}'";
+        $res      = mysql_query($control) or die ("SQL error:".mysql_error());
+        $rows     = mysql_num_rows($res);
 
-    	if($rows != 1)
+        if($rows != 1)
         {
-    		$query    = "INSERT INTO users (username,password,email,level) VALUES('$username','$password','$email','$level');";
+            $query    = "INSERT INTO users (username,password,email,level) VALUES('$username','$password','$email','$level');";
 
-    		$res      = mysql_query($query) or die ("SQL error:".mysql_error());
+            $res      = mysql_query($query) or die ("SQL error:".mysql_error());
 
-    		if ($res)
-    		{
-    			print "User registration = TRUE :), yep";
-    		}
+            if ($res)
+            {
+                print "User registration = TRUE :), yep";
+            }
 
-    		else
-    		{
-    			print "Trouble registering";
-    		}
+            else
+            {
+                print "Trouble registering";
+            }
         }
 
-    	else {
-    		print "Username or password that is' present";
-    	}
+        else
+        {
+            print "Username or password that is' present";
+        }
     }
 
 
