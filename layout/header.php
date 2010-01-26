d1a125d645bf0eb7b24964d480f1f2bd50a3062a
FL3 <fl3@FL3.homenet.telecomitalia.it>
2010-01-26 15:42:37 +0100

    add mysql_connect

--- layout/header.php
+++ layout/header.php
@@ -2,5 +2,8 @@
 
     include('config.php');
     include('sources/core.php');
+    
+    $connect = mysql_connect($host,$username,$password) or die ("SQL error:".mysql_error());
+    mysql_select_db($database) or die ("SQL error:".mysql_error());
 
 ?>
