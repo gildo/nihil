77ec82e47a2c8557a14f64d636ff5c2a1bf85c05
Fyskij <fyskij@gmail.com>
2010-01-26 19:10:20 +0100

    yep

--- layout/header.php
+++ layout/header.php
@@ -1,8 +1,8 @@
 <?php
 
-    include('config.php');
-    include('sources/core.php');
-    
+    define('ROOT_PATH', dirname(__FILE__));
+    include(ROOT_PATH.'/../config.php');
+    include(ROOT_PATH.'/../sources/core.php');
     $connect = mysql_connect($host,$username,$password) or die ("SQL error:".mysql_error());
     mysql_select_db($database) or die ("SQL error:".mysql_error());
 
