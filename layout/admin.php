07dbbd76bbfce4e646ca93d1e1eb9c89e7991ec3
Fyskij <fyskij@gmail.com>
2010-01-28 15:52:47 +0100

    idented

--- layout/admin.php
+++ layout/admin.php
@@ -2,60 +2,60 @@
 
     include('../layout/header.php');
 
-	if(is_logged() == TRUE) {
-    	
-		if(is_admin() == TRUE){
-			$mode = $_GET['mode'];
+    if(is_logged() == TRUE) {
 
-			switch ($mode)
-			{
-				case "post":
+        if(is_admin() == TRUE){
+            $mode = $_GET['mode'];
 
-				print "<form action = 'admin.php' method = 'POST' name='posta'>";
-				print "Author: <input type = 'text' name = 'author'><br>";
-				print "Name: <input type = 'text' name = 'name'><br>";
-				print "<textarea name = 'content' rows = '10' cols = '75'></textarea><br>";
-				print "<input type = 'submit' value = 'post'>";
-				print "</form>";
+            switch ($mode)
+            {
+                case "post":
 
-            	if (!empty ($_REQUEST ['author']) && !empty ($_REQUEST ['name']) && !empty ($_REQUEST ['content']))
+                print "<form action = 'admin.php' method = 'POST' name='posta'>";
+                print "Author: <input type = 'text' name = 'author'><br>";
+                print "Name: <input type = 'text' name = 'name'><br>";
+                print "<textarea name = 'content' rows = '10' cols = '75'></textarea><br>";
+                print "<input type = 'submit' value = 'post'>";
+                print "</form>";
+
+                if (!empty ($_REQUEST ['author']) && !empty ($_REQUEST ['name']) && !empty ($_REQUEST ['content']))
                 {
-					$author  =  $_POST['author'];
-					$content =  $_POST['content'];
-					$name    =  $_POST['name'];
-					$date    =  date ("d:m:y");
-					$hour    =  date ("H:i:s");
-                    	
-					post($author,$name,$content,$hour,$date);
-                		
-				}
-				else
-				{
-					print "Fill in all fields";
-				}               
+                    $author  =  $_POST['author'];
+                    $content =  $_POST['content'];
+                    $name    =  $_POST['name'];
+                    $date    =  date ("d:m:y");
+                    $hour    =  date ("H:i:s");
 
-				break;
+                    post($author,$name,$content,$hour,$date);
 
-			default:
-        
-			die ();
-		    
-			break;
+                }
+                else
+                {
+                    print "Fill in all fields";
+                }
 
-			}
-		}
-		else
-		{
-			print "Not have permission to view these pages :@";
-			header("Location: 3;URL=/index.php");
-		}
-	
-	}
-	else
-	{
-		print "Not logged";
-		header("Location: 3;URL=login.php");
-	}
+                break;
+
+            default:
+
+            die ();
+
+            break;
+
+            }
+        }
+        else
+        {
+            print "Not have permission to view these pages :@";
+            header("Location: 3;URL=/index.php");
+        }
+
+    }
+    else
+    {
+        print "Not logged";
+        header("Location: 3;URL=login.php");
+    }
 
     include('../layout/footer.php');
 
