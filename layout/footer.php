ea603861df670df6be68113985907b69da09fb99
Fyskij <fyskij@gmail.com>
2010-02-01 14:41:27 +0100

    updated readme

--- layout/footer.php
+++ layout/footer.php
@@ -5,9 +5,9 @@
 ?>
         </div>
         <div class="footer">
-        copyleft <a href="http://github.com/fyskij/NOcms">NOcms.</a><br>
-        Valid: <a href="http://validator.w3.org/check?uri=http://fl3.gotdns.org">W3C HTML</a>
-        <a href="http://jigsaw.w3.org/css-validator/validator?uri=http://fl3.gotdns.org">W3C CSS</a>
+        copyleft <a href="http://github.com/fyskij/lulz">lulz.</a><br>
+        Valid: <a href="http://validator.w3.org/check?uri=http://">W3C HTML</a>
+        <a href="http://jigsaw.w3.org/css-validator/validator?uri=http://">W3C CSS</a>
         </div>
     </body>
 </html>
